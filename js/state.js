fd1867ffe7f7c8a4b7927320cb285ac6526c50ef
Ivan Sidorin <ivsid8@yandex.ru>
2023-10-05 14:19:25 +0300

    first commit

---
+++ js/state.js
@@ -0,0 +1,19 @@
+export default {
+  url: "https://v6.exchangerate-api.com/v6/9fbb981041f5eec9370a7947",
+  codes: [],
+  pair: {
+    from: "",
+    to: "",
+  },
+  amount: "",
+  loading: false,
+  currentTab: "convert",
+  currency: {
+    code: "USD",
+  },
+  currencies: ["USD", "EUR", "BYN"],
+  actions: {
+    remove: "remove",
+    change: "change",
+  },
+};
